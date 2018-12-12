65a2594a1d4ac400e695af69661d06314b78e1f5
Sam Lu <ulmas@umich.edu>
2018-12-11 19:55:19 -0500

    adding login stuff

--- SI364final.py
+++ SI364final.py
@@ -53,6 +53,31 @@ def get_or_create_location(city, state):
 ##### MODELS #####
 ##################
 
+user_collection = db.Table('user_collection',db.Column('gas_id',db.Integer,db.ForeignKey('Gassy.gasid')),db.Column('collection_id',db.Integer,db.ForeignKey('PersonalCollection.id')))
+
+# Special model for users to log in
+class User(UserMixin, db.Model): # REMEMBER THAT YOU NEED TO CHANGE THIS DB RELATIONSHIP THING
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(255), unique=True, index=True)
+	email = db.Column(db.String(64), unique=True, index=True)
+	password_hash = db.Column(db.String(128))
+	collection = db.relationship("PersonalCollection", backref="Users") # one user, many personal collections of gifs with different names -- say, "Happy Gif Collection" or "Sad Gif Collection"
+	# Remember, the best way to do so is to add the field, save your code, and then create and run a migration!
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+## DB load function - Necessary for behind the scenes login manager that comes with flask_login capabilities! Won't run without this.
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id)) # returns User object or None
+
 class Gassy(db.Model): #each individual gas station has one location
     __tablename__ = "gasstations"
     gasid = db.Column(db.Integer, primary_key=True)
@@ -75,6 +100,16 @@ class Locations(db.Model): # one location can have many gas stations
     def __repr__(self):
         return "{}, {} (ID: {})".format(self.city, self.state, self.locationid)
 
+# Model to store a personal gif collection
+class PersonalCollection(db.Model):
+	__tablename__ = "PersonalGifCollection"
+	# TODO 364: Add code for the PersonalGifCollection model such that it has the following fields:
+	id = db.Column(db.Integer, primary_key=True)        # id (Integer, primary key)
+	title = db.Column(db.String(255))      # name (String, up to 255 characters)
+	userid = db.Column(db.Integer, db.ForeignKey('users.id'))    # one-to-many relationship with the User model (one user, many personal collections of stations with different names)
+	gifs = db.relationship('Gassy',secondary=user_collection,backref=db.backref("PersonalCollection",lazy='dynamic'),lazy='dynamic')
+	# many to many rselationship with the gassy model (one station in many personal collections, one personal collection has many stations in it).
+
 class Opinion(db.Model): #a table that is filled with user opinions on gas stations etc
     __tablename__ = "opinion"
     opinionid = db.Column(db.Integer, primary_key=True)
@@ -88,6 +123,33 @@ class Opinion(db.Model): #a table that is filled with user opinions on gas stati
 ###################
 ###### FORMS ######
 ###################
+
+class RegistrationForm(FlaskForm):
+	email = StringField('Email:', validators=[Required(),Length(1,64),Email()])
+	username = StringField('Username:',validators=[Required(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'Usernames must have only letters, numbers, dots or underscores')])
+	password = PasswordField('Password:',validators=[Required(),EqualTo('password2',message="Passwords must match")])
+	password2 = PasswordField("Confirm Password:",validators=[Required()])
+	submit = SubmitField('Register User')
+
+	#Additional checking methods for the form
+	def validate_email(self,field):
+		if User.query.filter_by(email=field.data).first():
+			raise ValidationError('Email already registered.')
+
+	def validate_username(self,field):
+		if User.query.filter_by(username=field.data).first():
+			raise ValidationError('Username already taken')
+
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators=[Required(), Length(1,64), Email()])
+	password = PasswordField('Password', validators=[Required()])
+	remember_me = BooleanField('Keep me logged in')
+	submit = SubmitField('Log In')
+
+class CollectionCreateForm(FlaskForm):
+	name = StringField('Collection Name',validators=[Required()])
+	gif_picks = SelectMultipleField('Stations to include')
+	submit = SubmitField("Create Collection")
 
 class PlaceForm(FlaskForm):
     location = StringField("Please enter the place you want to search for — ideally, city. ", validators=[Required(), Length(min=0,  max=64)])
@@ -123,6 +185,40 @@ def internal_server_error(e):
 #######################
 ###### VIEW FXNS ######
 #######################
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(email=form.email.data).first()
+		if user is not None and user.verify_password(form.password.data):
+			login_user(user, form.remember_me.data)
+			return redirect(request.args.get('next') or url_for('index'))
+		flash('Invalid username or password.')
+	return render_template('login.html',form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('You have been logged out')
+	return redirect(url_for('index'))
+
+@app.route('/register',methods=["GET","POST"])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('You can now log in!')
+		return redirect(url_for('login'))
+	return render_template('register.html',form=form)
+
+@app.route('/secret')
+@login_required
+def secret():
+	return "Only authenticated users can do this! Try to log in or contact the site admin."
 
 @app.route('/')
 def index():
